b0361a0810966348ea9c98e0efeaf092d3b88c72
Israel Sanchez <israelsanchez@Israels-MBP.home>
2019-05-15 13:57:45 +0200

    version 1 con final

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 class Circuito():
     corredores = []
@@ -17,7 +18,7 @@ class Circuito():
         
     # se invoca los objetos corredores
     
-        self._crearCorredores()
+        self.__crearCorredores()
     
     # aqui se crean los corredores
     
@@ -29,10 +30,22 @@ class Circuito():
                 new_turtle.penup()
                 new_turtle.setpos(self.__starLine, self.__posStarY[i])
                 
-                
                 self.corredores.append(new_turtle)
                 
-    
+    def competir(self):
+        
+        ganador = False
+        
+        while not ganador:
+            for tortuga in self.corredores:
+                avance = random.randint(1, 6)
+                tortuga.forward(avance)
+                
+                if tortuga.position()[0] >= self.__finishLine:
+                    ganador = True
+                    print("la tortuga de color {} ha ganado".format(tortuga.color()[0]))
+                    break
         
 if __name__ == '__main__':
     circuito = Circuito(640, 480)
+    circuito.competir()
